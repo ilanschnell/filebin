27af2482ca1bcc1fa0f9d97c4c5e72f04f5430c6
Ilan Schnell <ischnell@enthought.com>
2011-03-06 22:06:45 -0600

    cleanup

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ void process_options(int argc, char *argv[])
 {
     int op;
 
-    while((op=getopt(argc, argv, "nthv")) != -1)
+    while ((op = getopt(argc, argv, "nthv")) != -1)
         switch(op) {
         case 'n':
             names = table = 1;
@@ -56,7 +56,7 @@ void process_fp(FILE *fp)
 {
     int c;
 
-    while((c = fgetc(fp)) != EOF) {
+    while ((c = fgetc(fp)) != EOF) {
         bin[c]++;
         n++;
     }
@@ -67,7 +67,7 @@ void process_file(char *fname)
     FILE *fp;
 
     fp = fopen(fname, "rb");
-    if(fp == NULL) {
+    if (fp == NULL) {
         printf("Error: Can't open `%s' for reading.\n", fname);
         exit(1);
     }
@@ -85,11 +85,11 @@ char *name(int c)
 
     strcpy(res, ".");  /* default */
 
-    if(c >= 33 && c <= 126) {
+    if (c >= 33 && c <= 126) {
         res[0] = (char)c;
         res[1] = '\0';
     }
-#define M(_c, _name)  else if(c==_c) strcpy(res, "<" _name ">")
+#define M(_c, _name)  else if (c==_c) strcpy(res, "<" _name ">")
     M(0, "Null");
     M(9, "Tab");
     M(10, "LF");
@@ -104,11 +104,11 @@ void showTable()
 {
     int c;
 
-    if(names)
+    if (names)
         printf("Char               Count\n"
                "------------------------\n");
 
-    for(c=0; c<256; c++)
+    for (c = 0; c < 256; c++)
         if (bin[c]) {
             if(names)
                 printf("%4d %8s %10lld\n", c, name(c), bin[c]);
@@ -123,7 +123,8 @@ void showSum()
     int c;
 
     a = 0;
-    for(c=32; c<=126; c++) a += bin[c];
+    for (c = 32; c <= 126; c++)
+        a += bin[c];
     printf("ASCII ( 32..126 )........  :%12lld\n", a);
     printf("   0 (Null)............... :%12lld\n", bin[0]);
     printf("   9 (Tab)................ :%12lld\n", bin[9]);
@@ -132,18 +133,18 @@ void showSum()
     printf(" 127 (Del) ............... :%12lld\n", bin[127]);
 
     b1 = bin[11]+bin[12];
-    for(c= 1; c<= 8; c++) b1+=bin[c];
-    for(c=14; c<=31; c++) b1+=bin[c];
+    for(c = 1; c <=  8; c++) b1 += bin[c];
+    for(c =14; c <= 31; c++) b1 += bin[c];
     printf("Others (1..8,11,12,14..31) :%12lld\n", b1);
 
     b2 = 0;
-    for(c=128; c<=255; c++) b2+=bin[c];
+    for(c = 128; c <= 255; c++) b2 += bin[c];
     printf("Others (128..255) ........ :%12lld\n", b2);
 
     printf("                             -----------\n");
     printf("Total .................... :%12lld\n", n);
 
-    assert(n == a+bin[0]+bin[9]+bin[10]+bin[13]+bin[127]+b1+b2);
+    assert(n == a + bin[0] + bin[9] + bin[10] + bin[13] + bin[127] + b1 + b2);
 }
 
 int main(int argc, char *argv[])
@@ -153,18 +154,18 @@ int main(int argc, char *argv[])
     process_options(argc, argv);
 
     /* Initialize array */
-    for(c = 0; c < 256; c++)
+    for (c = 0; c < 256; c++)
         bin[c] = 0;
 
-    if(help)
+    if (help)
         usage(argv[0]);
-    else if(argc == optind)
+    else if (argc == optind)
         process_fp(stdin);
     else
-        while(optind < argc)
+        while (optind < argc)
             process_file(argv[optind++]);
 
-    if(table)
+    if (table)
         showTable();
     else
         showSum();
